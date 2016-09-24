02917a78975c9b86ad8fae5a6aaaefa10ce6c610
Erik Colban <erik.colban@jointheleague.org>
2016-09-24 09:02:49 -0700

    Add special numbers. Modify dobble.

--- dobble.py
+++ dobble.py
@@ -1,18 +1,29 @@
-#!/usr/bin/python
+p = 7
 
-p = 17
+gf = range(p)
 
-nums = range(p)
-points = [(x, y) for x in nums for y in nums]
-dirs = [(1, v) for v in nums] + [(0, 1)]
-lines = [(a, b, c) for (a,b) in dirs for c in nums]
-line_num = {line:n for n, line in enumerate(lines, 1)}
-dobble = [tuple(line_num[(a, b, (a * x + b * y) % p)] for (a, b) in dirs) for (x, y) in points]
-dobble.extend(tuple(line_num[(a, b, c)] for c in nums) + (len(lines) + 1,) for (a, b) in dirs)
+points = {(x, y) for x in gf for y in gf}
 
-def mkstr(iterable, sep, start='', end=''):
-    return '%s%s%s' % (start, sep.join(map(str, iterable)), end)
+directions = {(1, y) for y in gf} | {(0, 1)}
 
-for t in dobble:
-    print(mkstr(map(lambda n: '%02d' % n, t), ', ', start='[', end =']'))
+lines = {(a, b, c) for (a, b) in directions for c in gf}
 
+line_nums = {line:n for n, line in enumerate(lines)}
+
+cards_fin = {tuple(line_nums[(a, b, (a * x + b * y) % p)] for (a, b) in directions) for (x, y) in points}
+
+line_at_infinity = len(line_nums)
+
+cards_inf = {tuple(line_nums[(a, b, c)] for c in gf) + (line_at_infinity,) for (a, b) in directions}
+
+cards = cards_fin | cards_inf
+
+#====================================================
+# OUTPUT
+#====================================================
+
+# pix = [chr(i) for i in range(65, 91)] + [chr(i) for i in range(97, 123)] + list('@#$%&')
+# for c in cards: print(' '.join(pix[i] for i in c))
+
+frmt = '%%0%dd' % len(str(line_at_infinity))
+for c in cards: print(' '.join(frmt % i for i in c))
